8671abaf908764040b5399f78399d6c41b7c6e44
SallesCosta <newcapital.in@gmail.com>
2021-08-08 21:57:20 +0200

    fix index.js

--- index.js
+++ index.js
@@ -1,42 +1,4 @@
 const express = require('express');
-const  bodyParser = require('body-parser');
-const path = require('path');
-
-const app = express();
-
-app.use( bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.use('/public',express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, '/pages'));
-
-
-app.get('/',(req,res)=>{
-    console.log(req.query)
-    if(req.query.novapagina == null){
-        res.render('home', {});
-    }else{
-        res.send('Você buscou: '+req.query.novapagina);
-        /* http://localhost:5000/?novapagina=id3 */
-    }
-})
-
-app.get('/:slug',(req,res)=>{
-//Aula `Seu primeiro projeto NodeJS
-    res.send(req.params.slug);  /* localhost:5000/pagina2    sera renderizado pagina2 no html*/
-})
-
-app.listen(5000,()=>{
-    console.log('servidando FakanaKavera')
-}) 
-
-
-/* const express = require('express');
 const mongoose = require('mongoose');
 var bodyParser = require('body-parser')
 
@@ -46,16 +8,16 @@ const app = express();
 
 const Posts = require('./Posts.js');
 
-mongoose.connect('SEUSDADOSAQUI',{useNewUrlParser: true, useUnifiedTopology: true}).then(function(){
+mongoose.connect('SEUSDADOSAQUI', { useNewUrlParser: true, useUnifiedTopology: true }).then(function () {
     console.log('Conectado com sucesso');
-}).catch(function(err){
+}).catch(function (err) {
     console.log(err.message);
 })
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
+    extended: true
+}));
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -63,113 +25,81 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/pages'));
 
 
-app.get('/',(req,res)=>{
-    
-    if(req.query.busca == null){
-        Posts.find({}).sort({'_id': -1}).exec(function(err,posts){
-           // console.log(posts[0]);
-            posts = posts.map(function(val){
-                    return {
-                        titulo: val.titulo,
-                        conteudo: val.conteudo,
-                        descricaoCurta: val.conteudo.substr(0,100),
-                        imagem: val.imagem,
-                        slug: val.slug,
-                        categoria: val.categoria
-                        
-                    }
-            })
-
-            
-            Posts.find({}).sort({'views': -1}).limit(3).exec(function(err,postsTop){
-                // console.log(posts[0]);
-                 postsTop = postsTop.map(function(val){
-                         return {
-                             titulo: val.titulo,
-                             conteudo: val.conteudo,
-                             descricaoCurta: val.conteudo.substr(0,100),
-                             imagem: val.imagem,
-                             slug: val.slug,
-                             categoria: val.categoria,
-                             views: val.views
-                             
-                         }
-                 })
-
-                 
-
-                 res.render('home',{posts:posts,postsTop:postsTop});
-                
-             })
-
-             
-
-            
-        })
-        
-    }else{
-
-        Posts.find({titulo: {$regex: req.query.busca,$options:"i"}},function(err,posts){
-            console.log(posts);
-            posts = posts.map(function(val){
+app.get('/', (req, res) => {
+    if (req.query.busca == null) {
+        Posts.find({}).sort({ '_id': -1 }).exec(function (err, posts) {
+            // console.log(posts[0]);
+            posts = posts.map(function (val) {
                 return {
                     titulo: val.titulo,
                     conteudo: val.conteudo,
-                    descricaoCurta: val.conteudo.substr(0,100),
+                    descricaoCurta: val.conteudo.substr(0, 100),
+                    imagem: val.imagem,
+                    slug: val.slug,
+                    categoria: val.categoria
+                }
+            })
+            Posts.find({}).sort({ 'views': -1 }).limit(3).exec(function (err, postsTop) {
+                // console.log(posts[0]);
+                postsTop = postsTop.map(function (val) {
+                    return {
+                        titulo: val.titulo,
+                        conteudo: val.conteudo,
+                        descricaoCurta: val.conteudo.substr(0, 100),
+                        imagem: val.imagem,
+                        slug: val.slug,
+                        categoria: val.categoria,
+                        views: val.views
+                    }
+                })
+                res.render('home', { posts: posts, postsTop: postsTop });
+            })
+        })
+    } else {
+        Posts.find({ titulo: { $regex: req.query.busca, $options: "i" } }, function (err, posts) {
+            console.log(posts);
+            posts = posts.map(function (val) {
+                return {
+                    titulo: val.titulo,
+                    conteudo: val.conteudo,
+                    descricaoCurta: val.conteudo.substr(0, 100),
                     imagem: val.imagem,
                     slug: val.slug,
                     categoria: val.categoria,
                     views: val.views
-                    
                 }
+            })
+            res.render('busca', { posts: posts, contagem: posts.length });
         })
-            res.render('busca',{posts:posts,contagem:posts.length});
-        })
-
-
-        
     }
-
-  
 });
 
-
-app.get('/:slug',(req,res)=>{
+app.get('/:slug', (req, res) => {
     //res.send(req.params.slug);
-    Posts.findOneAndUpdate({slug: req.params.slug}, {$inc : {views: 1}}, {new: true},function(err,resposta){
-       // console.log(resposta);
-       if(resposta != null){
-
-        Posts.find({}).sort({'views': -1}).limit(3).exec(function(err,postsTop){
-            // console.log(posts[0]);
-             postsTop = postsTop.map(function(val){
-                     return {
-                         titulo: val.titulo,
-                         conteudo: val.conteudo,
-                         descricaoCurta: val.conteudo.substr(0,100),
-                         imagem: val.imagem,
-                         slug: val.slug,
-                         categoria: val.categoria,
-                         views: val.views
-                         
-                     }
-             })
-
-             res.render('single',{noticia:resposta,postsTop:postsTop});
-
+    Posts.findOneAndUpdate({ slug: req.params.slug }, { $inc: { views: 1 } }, { new: true }, function (err, resposta) {
+        // console.log(resposta);
+        if (resposta != null) {
+            Posts.find({}).sort({ 'views': -1 }).limit(3).exec(function (err, postsTop) {
+                // console.log(posts[0]);
+                postsTop = postsTop.map(function (val) {
+                    return {
+                        titulo: val.titulo,
+                        conteudo: val.conteudo,
+                        descricaoCurta: val.conteudo.substr(0, 100),
+                        imagem: val.imagem,
+                        slug: val.slug,
+                        categoria: val.categoria,
+                        views: val.views
+                    }
+                })
+                res.render('single', { noticia: resposta, postsTop: postsTop });
             })
-
-
-        
-       }else{
-           res.redirect('/');
-       }
+        } else {
+            res.redirect('/');
+        }
     })
-    
 })
 
-
-
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log('server rodando!');
-}) */
+})
