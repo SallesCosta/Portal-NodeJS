8b3c34678540cc7e2f348e921e5137a4df7bba10
SallesCosta <newcapital.in@gmail.com>
2021-08-08 13:07:32 +0200

    adaptando

---
+++ posts.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var postSchema = new Schema({
+    titulo: String,
+    imagem: String,
+    categoria: String,
+    conteudo: String,
+    slug: String,
+    autor: String,
+    views: Number,
+},{collection: 'posts'})//este postes é o nome da collection que criei no mongodb
+
+var Posts = mongoose.model("Posts",postSchema);
+module.exports = Posts;
